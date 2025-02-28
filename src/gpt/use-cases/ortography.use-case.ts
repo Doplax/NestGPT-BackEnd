0b9968e6b7a8889bb783322115ecc5f44ace4b41
Doplax <doplax@gmail.com>
2025-02-28 13:01:45 +0100

    feat: Spell checker

--- src/gpt/use-cases/ortography.use-case.ts
+++ src/gpt/use-cases/ortography.use-case.ts
@@ -12,13 +12,33 @@ export const ortographyCheckUseCase = async (
   const completion = await openai.chat.completions.create({
     model: 'gpt-4o-mini',
     messages: [
-      { role: 'system', content: "Tu nombre es pedro" },
+      {
+        role: 'system',
+        content: `
+        Te serán proveídos textos en español con posibles errores ortográficos y gramaticales,
+        Las palabras usadas deben de existir en el diccionario de la RAE,
+        Debes de responder en formato JSON,
+        tu tarea es corregirlos y retornar información de las solicitudes,
+        tambien debes de dar un porcentaje de acierto por el usuario,
+        
+        Si no hay errores debes de retornar un mensaje de felicitaciones
+
+        Ejemplo:
+        {
+          userScore:number,
+          errors: string[], // ['error => solución']
+          message: string // Usa emojis para dar feedback
+        }
+        `,
+      },
       { role: 'user', content: prompt },
     ],
     store: true,
+    max_tokens: 100,
+    temperature: 0.3,
   });
 
-  return completion.choices[0].message
+  return completion.choices[0].message;
 
   //return { prompt: prompt, apiKey: process.env.OPEN_API_KEY };
 };
