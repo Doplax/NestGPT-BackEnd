37e1fbdf4a5e1c94b718805b41f0a0382e01f5e5
Doplax <doplax@gmail.com>
2025-03-12 15:52:31 +0100

    feat: Saving the images in FileSystem

---
+++ src/gpt/helpers/download-image-as-png.ts
@@ -0,0 +1,21 @@
+import * as path from 'path';
+import * as fs from 'fs';
+
+import { InternalServerErrorException } from "@nestjs/common";
+
+export const donwloadImageAsPng = async (url: string) => {
+  const response = await fetch(url);
+
+  if (!response.ok) {
+    throw new InternalServerErrorException('Error downloading image');
+  }
+
+
+  const folderPath = path.resolve('./','./generated/images/');
+  fs.mkdirSync(folderPath , { recursive: true }); // Create folder if not exists
+ 
+  const imageNamePng = `${Date.now()}.png`;
+  const buffer = Buffer.from(await response.arrayBuffer());
+
+  fs.writeFileSync( `${folderPath}/${imageNamePng}`, buffer);
+}
--- src/gpt/use-cases/image-generation.use-case.ts
+++ src/gpt/use-cases/image-generation.use-case.ts
@@ -1,5 +1,6 @@
 import OpenAI from 'openai';
 import * as fs from 'fs';
+import { donwloadImageAsPng } from '../helpers';
 
 interface Options {
   prompt: string;
@@ -7,9 +8,27 @@ interface Options {
   maskImage?: string;
 }
 
+
 export const imageGenerationUseCase = async (openai: OpenAI, options: Options) => {
 
   const { prompt, originalImage, maskImage } = options;
   
+  // Todo: Verificar original image
+  const response = await openai.images.generate({
+    prompt: prompt,
+    model: 'dall-e-3',
+    n: 1,
+    size: '1024x1024',
+    quality: 'standard',
+    response_format: 'url',
 
+  });
+
+  donwloadImageAsPng('https://oaidalleapiprodscus.blob.core.windows.net/private/org-wib0f3HHqIWxIUKcNH07WzsV/user-pqrZ5dioFEpSJZybFfgwcm4i/img-kTarNyutiYMDblM9mn1mW9ki.png?st=2025-03-12T13%3A12%3A51Z&se=2025-03-12T15%3A12%3A51Z&sp=r&sv=2024-08-04&sr=b&rscd=inline&rsct=image/png&skoid=d505667d-d6c1-4a0a-bac7-5c84a87759f8&sktid=a48cca56-e6da-484e-a814-9c849652bcb3&skt=2025-03-11T23%3A16%3A45Z&ske=2025-03-12T23%3A16%3A45Z&sks=b&skv=2024-08-04&sig=zLO3kPzHU9q/7wSD7pRqn%2Bfwct5O3//0irN1PtU43d0%3D');
+ 
+  return {
+    url: response.data[0].url,
+    localPath: '',
+    revised_prompt: response.data[0].revised_prompt,
+  }
 };
