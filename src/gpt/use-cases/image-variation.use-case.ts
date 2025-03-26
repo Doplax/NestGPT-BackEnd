1ef130b0534aea24dbfde9c9e6f7bc8d755333e8
Doplax <doplax@gmail.com>
2025-03-26 12:43:12 +0100

    feat: Implement image variation

--- src/gpt/use-cases/image-variation.use-case.ts
+++ src/gpt/use-cases/image-variation.use-case.ts
@@ -2,6 +2,7 @@ import OpenAI from 'openai';
 import * as fs from 'fs';
 import * as path from 'path';
 import { donwloadImageAsPng, downloadBase64ImageAsPng } from '../helpers';
+import { url } from 'inspector';
 
 interface Options {
   baseImage: string;
@@ -12,5 +13,24 @@ export const imageVariationUseCase = async (
   options: Options,
 ) => {
   const { baseImage } = options;
-  console.log(baseImage);
+
+  const pngImageFullPath = await donwloadImageAsPng(baseImage, true);
+  console.log(pngImageFullPath);
+
+  const response = await openai.images.createVariation({
+    model: 'dall-e-3',
+    image: fs.createReadStream(pngImageFullPath),
+    n:1,
+    size: '1024x1024',
+    response_format: 'url',
+  });
+
+  const fileName = await donwloadImageAsPng( response.data[0].url );
+  const url = `${ process.env.BASE_URL }/gpt/image-generation/${fileName}`;
+
+  return { 
+    url: url,
+    openAIUrl: response.data[0].url,
+    revised_prompt: response.data[0].revised_prompt
+  }
 };
