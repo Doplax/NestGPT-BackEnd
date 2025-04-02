5966e2382ea2d0a37bf95ededa770d587a4af34a
Doplax <doplax@gmail.com>
2025-04-02 11:33:03 +0200

    feat : Create Thread - Use Case

--- src/sam-assistant/sam-assistant.controller.ts
+++ src/sam-assistant/sam-assistant.controller.ts
@@ -9,7 +9,7 @@ export class SamAssistantController {
 
   @Post('create-thread')
   async createThread() {
-    return 'createThread';
+    return this.samAssistantService.createThread();
   }
 
   @Post('user-question')
--- src/sam-assistant/sam-assistant.service.ts
+++ src/sam-assistant/sam-assistant.service.ts
@@ -1,4 +1,19 @@
 import { Injectable } from '@nestjs/common';
 
+import OpenAI from 'openai';
+import { createThreadUseCase } from './use-cases/create-thread.use-case';
+
 @Injectable()
-export class SamAssistantService {}
+export class SamAssistantService {
+
+    private openai = new OpenAI({ apiKey: process.env.OPEN_API_KEY });
+
+
+    async createThread() {
+        return await createThreadUseCase(this.openai)
+    }
+    
+    userQuestion(questionDto) {
+        return questionDto;
+    }
+}
